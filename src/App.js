d48f855e369b08c4763ac582233f4431cce49f8a
dabuzida <dabuzida@gmail.com>
2022-01-29 15:56:34 +0900

    생활코딩 따라하기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 // import logo from './logo.svg';
 import React, { Component } from 'react';
+import Subject from "./components/Subject"
+import TOC from "./components/TOC"
+import Content from "./components/Content"
+
 import './App.css';
 
-
-class Subject extends Component{
-  render(){
-    return (
-      <header>
-            <h1>WEB</h1>
-            world wide web!
-        </header>
-
-    );
-  }
-}
-
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:"WEB", sub:"world wide web!"}
+    }
+  }
   render(){
     return (
       <div className="App">
-        <Subject></Subject>
+        <Subject 
+          title={this.state.subject.title} 
+          sub={this.state.subject.sub}
+        />
+        <TOC />
+        <Content title="HTML" desc="HTML is HyperText Markup Language." />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
