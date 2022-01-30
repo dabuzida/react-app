0078d528c9306ffdd7894d3cb3a48733d2216372
dabuzida <dabuzida@gmail.com>
2022-01-31 00:10:01 +0900

    component props state event bind...

--- src/App.js
+++ src/App.js
@@ -10,23 +10,71 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode: 'read',
+      selected_content_id:2,
       subject:{title:'WEB', sub:'world wide web!'},
+      welcome:{title:'Welcom', desc:'Hello, React!!'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
-        {id:3, title:'JavaScirpt', desc:'HTML is for interactive'}
+        {id:3, title:'JavaScirpt', desc:'JavaScirpt is for interactive'}
       ]
     }
   }
   render(){
+    var _title, _desc = null;
+    // let nowMode = this.state.mode;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      let i=0;
+      while(i < this.state.contents.length){
+        let data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+    }
     return (
       <div className="App">
+        {/* <header>
+          <h1><a href="/" onClick={function(e){
+            console.log(e);
+            e.preventDefault();
+            if(nowMode === 'welcome'){
+              this.setState({
+                mode:'read',
+              });
+            } else if(nowMode === 'read'){
+              this.setState({
+                mode:'welcome',
+              });
+            }
+          }.bind(this)}>{this.state.subject.title}</a></h1>
+          {this.state.subject.sub}
+        </header> */}
         <Subject 
           title={this.state.subject.title} 
           sub={this.state.subject.sub}
+          onNewEvent={function(){
+            this.setState({
+              mode:'welcome',
+            })
+          }.bind(this)}
         />
-        <TOC data={this.state.contents} />
-        <Content title="HTML" desc="HTML is HyperText Markup Language." />
+        <TOC 
+          onNewEvent={function(id){
+            this.setState({
+              mode:'read',
+              selected_content_id: +id,
+            })
+          }.bind(this)}
+          data={this.state.contents} />
+        <Content title={_title} desc={_desc} />
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
